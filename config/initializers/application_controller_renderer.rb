d01d670dadc8a78a259f934170faa6bf705eb536
m-HirokiOhara <ohara.hiroki.infratop@gmail.com>
2018-09-16 14:23:20 +0000

    second commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '9e0abf99b4477846aadd9170b7fe44d91f919236bad58c03cb1ef7706dbf62a90630dd4e6bfe93eb8b074a9287c0a60b0c66fef790fb6a7514e6635bcef4d3e4'
